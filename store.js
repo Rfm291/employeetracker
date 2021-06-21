e765ba63e38ae9805e56ca76c710c1d4000645d2
Richard Mercado <RichardFM291@gmail.com>
2021-06-21 15:38:01 -0400

    added initial code

---
+++ store.js
@@ -0,0 +1,15 @@
+const connection = require('./connection');
+
+class Store{
+    constructor(connection){
+        this.connection = connection;
+    }
+
+    read() {
+        return this.connection.query("SELECT * FROM employee;");
+    }
+    searchByname(name){
+        return this.connection.query("SELECT * FROM")
+    }
+}
+
