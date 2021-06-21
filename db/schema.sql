4634f8365a1d9da3da18d540ba0f99ed8d40db7c
Richard Mercado <RichardFM291@gmail.com>
2021-06-21 16:01:46 -0400

    added db format in mysql, added env

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,35 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+ -- id - INT PRIMARY KEY
+    id INT AUTO_INCREMENT,
+-- name - to hold department name
+    name - VARCHAR(30),
+    PRIMARY KEY(id)   
+);
+
+CREATE TABLE role(
+-- id - INT PRIMARY KEY
+    id INT AUTO_INCREMENT,
+-- name - to hold role title
+    name - VARCHAR(30),
+-- salary - DECIMAL to hold role salary
+    salary DECIMAL ,
+    department_id INT   
+);
+
+CREATE TABLE role(
+-- id - INT PRIMARY KEY
+    id INT PRIMARY KEY AUTO_INCREMENT,
+-- first name - to hold employee first name
+    first_name - VARCHAR(30),
+-- last name -  to hold employee last name
+    last_name - VARCHAR(30),
+-- role_id - to hold reference to role employee has
+    role_id - INT,
+-- manager_id  - to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+    manager_id - INT,
+);
